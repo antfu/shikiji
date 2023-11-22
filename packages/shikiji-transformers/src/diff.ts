e464a2000f76705ec0d14b501451a65c64d2748c
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-22 20:03:02 +0100

    chore: update

--- packages/shikiji-transformers/src/diff.ts
+++ packages/shikiji-transformers/src/diff.ts
@@ -15,9 +15,9 @@ export interface TransformerDiffNotationOptions {
    */
   classRemoved?: string
   /**
-   * Class added to the root element when the file has diff
+   * Class added to the root element when the current code has diff
    */
-  classHasDiff?: string
+  classRootActive?: string
 }
 
 /**
@@ -29,43 +29,35 @@ export function transformerDiffNotation(
   const {
     classAdded = 'diff added',
     classRemoved = 'diff removed',
-    classHasDiff = 'has-diff',
+    classRootActive = 'has-diff',
   } = options
 
   return {
     name: 'shikiji-transformers:diff-notation',
-    pre(code) {
-      let hasDiff = false
-      const codeEl = code.children[0] as Element
-      for (const line of codeEl.children) {
-        if (line.type !== 'element')
+    line(line) {
+      let nodeToRemove: Element | undefined
+      for (const child of line.children) {
+        if (child.type !== 'element')
           continue
-        let nodeToRemove: Element | undefined
-        for (const child of line.children) {
-          if (child.type !== 'element')
-            continue
-          if (!isCommentLike(child, line))
-            continue
-          const text = child.children[0]
-          if (text.type !== 'text')
-            continue
-          const match = text.value.match(regexMatch)
-          if (!match)
-            continue
-          const className = match[1] === '--'
-            ? classRemoved
-            : classAdded
-          nodeToRemove = child
-          addClassToHast(line, className)
-          hasDiff = true
-          break
-        }
-        if (nodeToRemove)
-          line.children.splice(line.children.indexOf(nodeToRemove), 1)
+        if (!isCommentLike(child, line))
+          continue
+        const text = child.children[0]
+        if (text.type !== 'text')
+          continue
+        const match = text.value.match(regexMatch)
+        if (!match)
+          continue
+        const className = match[1] === '--'
+          ? classRemoved
+          : classAdded
+        nodeToRemove = child
+        addClassToHast(line, className)
+        if (classRootActive)
+          addClassToHast(this.pre, classRootActive)
+        break
       }
-
-      if (hasDiff && classHasDiff)
-        addClassToHast(code, classHasDiff)
+      if (nodeToRemove)
+        line.children.splice(line.children.indexOf(nodeToRemove), 1)
     },
   }
 }
--- packages/shikiji-transformers/src/focus.ts
+++ packages/shikiji-transformers/src/focus.ts
@@ -13,7 +13,7 @@ export interface TransformerDiffNotationOptions {
   /**
    * Class added to the root element when the code has focused lines
    */
-  classRootHasFocus?: string
+  classRootActive?: string
 }
 
 /**
@@ -24,42 +24,34 @@ export function transformerFocusNotation(
 ): ShikijiTransformer {
   const {
     classFocused = 'focused',
-    classRootHasFocus = 'has-focused',
+    classRootActive = 'has-focused',
   } = options
 
   return {
     name: 'shikiji-transformers:focus-notation',
-    pre(code) {
-      let hasMatched = false
-      const codeEl = code.children[0] as Element
-      for (const line of codeEl.children) {
-        if (line.type !== 'element')
+    line(line) {
+      let nodeToRemove: Element | undefined
+      for (const child of line.children) {
+        if (child.type !== 'element')
           continue
-        let nodeToRemove: Element | undefined
-        for (const child of line.children) {
-          if (child.type !== 'element')
-            continue
-          if (!isCommentLike(child, line))
-            continue
-          const text = child.children[0]
-          if (text.type !== 'text')
-            continue
-          const match = text.value.match(regexMatch)
-          if (!match)
-            continue
-          if (match[1] === 'focus') {
-            nodeToRemove = child
-            addClassToHast(line, classFocused)
-            hasMatched = true
-            break
-          }
+        if (!isCommentLike(child, line))
+          continue
+        const text = child.children[0]
+        if (text.type !== 'text')
+          continue
+        const match = text.value.match(regexMatch)
+        if (!match)
+          continue
+        if (match[1] === 'focus') {
+          nodeToRemove = child
+          addClassToHast(line, classFocused)
+          if (classRootActive)
+            addClassToHast(this.pre, classRootActive)
+          break
         }
-        if (nodeToRemove)
-          line.children.splice(line.children.indexOf(nodeToRemove), 1)
       }
-
-      if (hasMatched && classRootHasFocus)
-        addClassToHast(code, classRootHasFocus)
+      if (nodeToRemove)
+        line.children.splice(line.children.indexOf(nodeToRemove), 1)
     },
   }
 }
