af99b2bb5b29fecec83c92e73066fbd367ad5903
Kawasaki Shizuku <starsmoe@outlook.com>
2024-01-21 00:52:54 +0800

    feat: translate search bar

--- docs/.vitepress/zh.ts
+++ docs/.vitepress/zh.ts
@@ -44,6 +44,30 @@ export const zhConfig = defineConfig({
     outline: {
       label: '目录',
     },
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
     nav: [
       {
         text: '指南',
